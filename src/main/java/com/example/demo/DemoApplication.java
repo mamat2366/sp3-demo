806e719ebf1622a8fa46ec1e9e5b8dd631802e1d
mac <15739564793@qq.cm>
2024-10-27 23:56:41 +0800

    新项目创建成功！！！

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //程序主入口
+//第二次提交
 @SpringBootApplication
 public class DemoApplication {
 
